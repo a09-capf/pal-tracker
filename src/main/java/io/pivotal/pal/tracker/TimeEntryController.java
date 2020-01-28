1514b4ad42e891ac4655fcc65ca1c8a16b9e90e8
Yusuke Sakai <a09.capf@gmail.com>
2020-01-28 11:19:27 +0900

    finish spring-mvc

---
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
@@ -0,0 +1,54 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/time-entries")
+public class TimeEntryController {
+    private final TimeEntryRepository timeEntryRepository;
+
+    public TimeEntryController(@Autowired TimeEntryRepository timeEntryRepository) {
+        this.timeEntryRepository = timeEntryRepository;
+    }
+
+
+    @PostMapping
+    public ResponseEntity create(@RequestBody TimeEntry timeEntryToCreate) {
+        TimeEntry createdTimeEntry = timeEntryRepository.create(timeEntryToCreate);
+        return new ResponseEntity(createdTimeEntry, HttpStatus.CREATED);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<TimeEntry> read(@PathVariable long id) {
+        TimeEntry readTimeEntry = timeEntryRepository.find(id);
+        if (readTimeEntry != null) {
+            return new ResponseEntity<>(readTimeEntry, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TimeEntry>> list() {
+        return new ResponseEntity<>(timeEntryRepository.list(), HttpStatus.OK);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity update(@PathVariable  long id, @RequestBody  TimeEntry timeEntry) {
+        TimeEntry updatedTimeEntry = timeEntryRepository.update(id, timeEntry);
+        if (updatedTimeEntry != null) {
+            return new ResponseEntity(updatedTimeEntry, HttpStatus.OK);
+        }
+        return new ResponseEntity(HttpStatus.NOT_FOUND);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity delete(@PathVariable  long id) {
+        timeEntryRepository.delete(id);
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+}
